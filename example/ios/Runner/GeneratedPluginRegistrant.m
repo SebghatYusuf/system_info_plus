c500e7c156d6a4c5025a8b5bbd69d0078229a7c1
sebghatullah <sebghatullah@empirepixel.com>
2022-01-09 20:43:59 +0430

    second commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<system_info_plus/SystemInfoPlusPlugin.h>)
+#import <system_info_plus/SystemInfoPlusPlugin.h>
+#else
+@import system_info_plus;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SystemInfoPlusPlugin registerWithRegistrar:[registry registrarForPlugin:@"SystemInfoPlusPlugin"]];
+}
+
+@end
